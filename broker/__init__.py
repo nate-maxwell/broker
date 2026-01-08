bfe373d11fd84a4632c43a0a3408fd654a7bca96
nate-maxwell <natedmaxwell@gmail.com>
2026-01-07 19:32:49 -0800

    updating version number

--- broker/__init__.py
+++ broker/__init__.py
@@ -48,8 +48,8 @@ from broker import exceptions
 from broker import subscriber
 
 
-version_major = 0
-version_minor = 1
+version_major = 1
+version_minor = 2
 version_patch = 0
 
 __version__ = f"{version_major}.{version_minor}.{version_patch}"
